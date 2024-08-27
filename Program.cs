abe8f6df5631273722798c6e401a2e56e1a9afd8
RaulRosas <raul.rosas92@hotmail.com>
2024-08-27 14:56:24 -0300

    program develop and functionality coverage.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+string binary;
+bool flag = false;
+int result;
+
+while (!flag)
+{
+    Console.WriteLine("Insert the binary number to be transformed to decimal:");
+    binary = Console.ReadLine();
+    if (string.IsNullOrEmpty(binary) || binary.Length > 8 || !Regex.IsMatch(binary, "^[0-1]")) {
+        Console.WriteLine("The inserted value must consist only of 0 and 1, and should be under 8 digits long.");
+        continue;
+    }
+    else {
+        result = Convert.ToInt32(binary, 2);
+        Console.WriteLine($"The converted decimal is: {result}");
+        flag = true;
+    }
+}
